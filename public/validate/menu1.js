7eaf7dbe6d07806531f5bca624a0427e1060e9c3
sabrynm98 <smaisonneuve@umass.edu>
2021-04-02 23:09:36 -0400

    js client validation menu1+menu2

--- public/validate/menu1.js
+++ public/validate/menu1.js
@@ -1,20 +1,28 @@
 // validate entree
-$("#entrees").on("change", checkEntree)
+$(document).ready(function(){
+    $("#menu").on("change", checkOrder)
 
-function checkEntree(){
-   let sum = 0;
-   let checkEntree = $('#entrees input[type="number"]');
-   for(let i=0; i<checkEntree.length; i++){
-       sum += parseInt($(checkEntree[i]).val()) || 0; 
-       console.log(sum) 
-   }
-   if(sum > 0){
-       $("#entreeWarning").hide();
-   }
-   else{
-       $("#entreeWarning").show();
-   }
-}
+    function checkOrder(){
+        let totalItems = 0;
+        let checkEntree = $('#entrees input[type="number"]');
+        let checkSides = $('#sidesDrinks input[type="number"]');
+
+        for(let i=0; i<checkEntree.length; i++){
+            for(let j=0; j<checkSides.length; j++){
+                totalItems += parseInt($(checkSides[j]).val()) || 0; 
+                console.log(checkSides[j]);
+            }
+            totalItems += parseInt($(checkEntree[i]).val()) || 0;
+        }
+
+        if(totalItems > 0){
+            $("#warning").hide();
+        }
+        else{
+            $("#warning").show();
+        }
+    }
+});
 
 // total cost
 // find a way to get the qty stored in a variable
