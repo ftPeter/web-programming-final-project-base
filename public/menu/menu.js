34b18d50a911d190b27199fe5b5d2588fb249cf7
awatterson22 <abbywatterson19@yahoo.com>
2021-04-04 01:37:15 -0400

    dynamically update total on menu pages

--- public/menu/menu.js
+++ public/menu/menu.js
@@ -1,28 +1,48 @@
-// validate entree
-$(document).ready(function(){
+let choseItem = false;
+
+$(document).ready(function () {
     $("#menu").on("change", checkOrder)
+    $('#menu').on("change", updateTotal)
+    $('#menu').submit(validateCheckOut)
 
-    function checkOrder(){
+    // validate entrees and sides
+    function checkOrder() {
         let totalItems = 0;
-        let checkEntree = $('#entrees input[type="number"]');
-        let checkSides = $('#sidesDrinks input[type="number"]');
+        let checkEntree = $('#entrees input[type="number"]').val();
+        let checkSides = $('#sidesDrinks input[type="number"]').val();
 
-        for(let i=0; i<checkEntree.length; i++){
-            for(let j=0; j<checkSides.length; j++){
-                totalItems += parseInt($(checkSides[j]).val()) || 0; 
-                console.log(checkSides[j]);
-            }
-            totalItems += parseInt($(checkEntree[i]).val()) || 0;
-        }
+        totalItems += checkEntree + checkSides;
 
-        if(totalItems > 0){
+        if (totalItems > 0) {
             $("#warning").hide();
+            choseItem = true;
         }
         else{
             $("#warning").show();
+            choseItem = false;
+        }
+        
+    }
+
+    function updateTotal() {
+        let price = 0;
+        let items = $('input[type="number"]');
+
+        $.each(items, function (index, value) {
+
+            if (parseInt($(value).val()) >= 1) {
+                val = parseInt($(value).val())
+                priceStr = value.nextSibling.nextSibling.nextSibling.nextSibling.innerText;
+                price += (val * parseFloat(priceStr.substring(1)));
+            }
+        });
+        $("#total")[0].innerText = "Order Total: $" + price.toFixed(2);
+    }   
+
+    // validate that entrees and sides were chosen
+    function validateCheckOut(event) {
+        if (!choseItem) {
+            event.preventDefault();
         }
     }
 });
-
-// total cost
-// find a way to get the qty stored in a variable
